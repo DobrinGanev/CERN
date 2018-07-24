90e95610258f1372c041e8cbe662c84b8d39bdbc
DobrinGanev <dobringanev@gmail.com>
2018-07-23 23:20:55 -0600

    clean this thing

--- client/baseComponents/Grid.js
+++ client/baseComponents/Grid.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './style.scss';
-function Grid({hello}) {
+function Grid({ hello }) {
   return (
     <div className='root'>
       <h1 className='title'>{hello}</h1>
